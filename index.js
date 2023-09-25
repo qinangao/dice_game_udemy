2520e8ea344b40dace82c1e11021ed30cde989bf
Qinan Gao <qinanagao@gmail.com>
2023-09-25 22:25:38 +0100

    update js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// Left Dice
 var randomNumber1 = Math.floor(Math.random() * 6 + 1);
-var randomImageName = 
+var randomImageName = "dice" + randomNumber1 + ".png";
+var randonImageLocation = "images/" + randomImageName;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randonImageLocation);
+
+// Right Dice
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+var randomImageName2 = "dice" + randomNumber2 + ".png";
+var randonImageLocation2 = "images/" + randomImageName2;
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randonImageLocation2);
