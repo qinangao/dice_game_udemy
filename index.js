004389d54a0d305e2c0ef31d976f998c70ef6a66
Qinan Gao <qinanagao@gmail.com>
2023-09-26 21:03:07 +0100

    finalised dice game

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ var randonImageLocation2 = "images/" + randomImageName2;
 
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randonImageLocation2);
+
+// Result
+
+if (randomNumber1 === randomNumber2) {
+  document.querySelector("h1").innerHTML = "Draw!";
+} else if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
